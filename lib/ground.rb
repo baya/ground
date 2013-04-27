e631099ab38752cc4e3a6c1cd47625708cbc65c7
jiangguimin <jiangguimin@shensen.magus-soft.com>
2013-04-27 16:19:30 +0800

    ground activity

---
+++ lib/ground.rb
@@ -0,0 +1,68 @@
+module Ground
+  class Activity
+    attr_reader :data
+    
+    def self.<< data
+      new(data).call
+    end
+
+    def self.data_reader *attrs
+      
+      attrs.each do |attr|
+        define_method attr do
+          instance_variable_get("@#{attr}") || \
+          instance_variable_set("@#{attr}", data[attr.to_sym])
+        end
+      end
+      
+    end
+
+    def self.default attr, default
+      orig_attr = "#{attr}_without_default"
+      alias_method orig_attr, attr
+      define_method attr do
+        send(orig_attr) || default
+      end
+    end
+
+    def self.set name, value
+
+      if value.is_a? Symbol
+        define_method name do
+          instance_variable_get("@#{name}") || \
+          instance_variable_set("@#{name}", send(value) )
+        end
+      elsif value.respond_to? :call
+        define_method name do
+          instance_variable_get("@#{name}") || \
+          instance_variable_set("@#{name}", value.call)
+        end
+      else
+        define_method name do
+          instance_variable_get("@#{name}") || \
+          instance_variable_set("@#{name}", value)
+        end
+      end
+      
+    end
+
+    def initialize(data)
+      @data = data
+    end
+    
+    def call
+      ''
+    end
+
+    private
+
+    def get_or_set(name)
+      value = instance_variable_get("@#{name}")
+      return value if value
+      value = yield
+      instance_variable_set("@#{name}", value)
+    end
+
+  end
+
+end
