949d6d929ee1fdc8b09e5a40c4a9e7f1ac8ed551
jiangguimin <jim@huarongshise.local>
2013-04-28 00:52:47 +0800

    activity test

---
+++ lib/ground/activity.rb
@@ -0,0 +1,53 @@
+module Ground
+  class Activity
+    attr_reader :data
+
+    def self.inherited(subclass)
+      return if Kernel.method_defined? subclass.name
+      
+      Kernel.class_eval %Q{
+        def #{subclass.name}(data = {})
+          #{subclass.name}.new(data).call
+        end
+      }
+      
+    end
+    
+    def self.<< data
+      new(data).call
+    end
+
+    def self.data_reader *attrs
+      attrs.each do |attr|
+        define_method(attr) { data[attr]}
+      end
+    end
+
+    def self.data_default attr, default
+      define_method(attr) { data[attr] || default}
+    end
+
+    def self.set name, value
+      define_method(name) { value }
+    end
+
+    def initialize(data)
+      @data = data
+    end
+    
+    def call
+      ''
+    end
+
+    private
+
+    def get_or_set(name)
+      value = instance_variable_get("@#{name}")
+      return value if value
+      value = yield
+      instance_variable_set("@#{name}", value)
+    end
+
+  end
+
+end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -1,5 +1,4 @@
-require 'test/unit'
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib/ground'))
+require 'test_helper'
 
 class A < Ground::Activity
 
@@ -17,6 +16,14 @@ module B
   end
 end
 
+module B
+  class H < Ground::Activity
+    def call
+      true
+    end
+  end
+end
+
 class C
   class A < Ground::Activity
     def call
@@ -25,13 +32,67 @@ class C
   end
 end
 
+class C
+  class D < Ground::Activity
+    def call
+      true
+    end
+  end
+end
+
+
+class JustReturnActivitySelf < Ground::Activity
+  data_reader :a, :b, :c, :d
+
+  data_default :a, 'a'
+  data_default :b, 'b'
+
+  set :name, 'name'
+
+  def call
+    self
+  end
+  
+end
+
 class ActivityTest < Test::Unit::TestCase
 
 
   def test_capital_method
     assert A()
     assert B::A()
+    assert C::D()
     assert C::A()
+    assert C::D()
+
+    assert A << {}
+    assert B::A << {}
+    assert B::H << {}
+    assert C::A << {}
+    assert C::D << {}
+  end
+
+  def test_data_reader
+    activity = JustReturnActivitySelf(a: 'a', b: 'b', c: 'c', d: 'd')
+    
+    assert_equal activity.a, 'a'
+    assert_equal activity.b, 'b'
+    assert_equal activity.c, 'c'
+    assert_equal activity.d, 'd'
+  end
+
+  def test_data_default
+    activity = JustReturnActivitySelf(c: 'c')
+
+    assert_equal activity.a, 'a'
+    assert_equal activity.b, 'b'
+    assert_equal activity.d, nil
+  end
+
+  def test_set
+    activity = JustReturnActivitySelf(a: 'a')
+
+    assert_equal activity.name, 'name'
   end
   
 end
