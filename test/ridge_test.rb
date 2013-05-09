3eaa515776c064b322fb50d93ac464ad2c0428d2
jiangguimin <jiangguimin@shensen.magus-soft.com>
2013-05-09 11:43:01 +0800

    Ridge routes

--- test/ridge_test.rb
+++ test/ridge_test.rb
@@ -7,10 +7,34 @@ module Ground
     data_reader :path, :verb
     data_default :verb, 'GET'
 
+
+    class << self
+
+      attr_reader :routes
+
+      def route(location, activity)
+        @routes ||= {}
+        @routes[location] = activity
+      end
+
+    end
+
+    
     def call(&p)
-      c = Class.new(Activity)
-      c.class_eval &p if block_given?
-      c
+      activity = Class.new(Activity)
+      route(activity)
+      activity.class_eval &p if block_given?
+      activity
+    end
+
+    private
+
+    def route(activity)
+      self.class.route(location, activity)
+    end
+
+    def location
+      "%s:@:%s" % [verb.upcase, path]
     end
     
   end
@@ -35,8 +59,10 @@ end
 
 class RidgeTest < Test::Unit::TestCase
 
-  def test_route
-    assert BookCreate << {name: 'name'}
+  def test_routes
+    assert Ground::Ridge.routes['GET:@:/books']    == BooksIndex
+    assert Ground::Ridge.routes['GET:@:/book/:id'] == BookShow
+    assert Ground::Ridge.routes['POST:@:/book']    == BookCreate
   end
   
 end
