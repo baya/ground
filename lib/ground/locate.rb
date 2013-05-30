3346e03034f568b53e6100d90165a80df7fcfd76
jiangguimin <jiangguimin@shensen.magus-soft.com>
2013-05-30 09:45:26 +0800

    locate adjust

--- lib/ground/locate.rb
+++ lib/ground/locate.rb
@@ -11,23 +11,24 @@ module Ground
     def call
       path_segs = path.split('/')
       path_segs[0] = '/'
-      match = false
       
       length_match_routes = Ridge.routes[verb].select {|route|
         route[0].split('/').size == path_segs.size
       }
 
       length_match_routes.detect {|route|
+        match = true
         segs = route[0].split('/')
         segs[0] = '/'
         segs.each_with_index {|seg, index|
           match = (path_segs[index] == seg)  if not seg =~ /^:\w+/
+          break if not match
         }
         match
       }
 
     end
 
-  end #Locate
+  end
   
-end #Ground
+end
