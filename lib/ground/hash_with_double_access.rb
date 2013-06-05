56cb0fe16f8d276119b58dc3f29dbde1d5c5d317
jiangguimin <jiangguimin@shensen.magus-soft.com>
2013-06-05 16:35:47 +0800

    hash with double access

--- lib/ground/hash_with_double_access.rb
+++ lib/ground/hash_with_double_access.rb
@@ -1,26 +1,26 @@
 class HashWithDoubleAccess < Ground::Activity
 
   def call
-    double_h = {}
-    data.each {|k, v|
-      if v.is_a?(Hash)
-        double_h[k.to_sym] = access_with(v, :to_sym)
-        double_h[k.to_s] =  access_with(v, :to_s)
-      else
-        double_h[k.to_sym] = double_h[k.to_s] =  v
-      end
-    }
-    double_h
+    str_h = access_with(data, :to_s)
+    sym_h = access_with(data, :to_sym)
+    sym_h.merge str_h
   end
 
 
   private
-  
-  def access_with(h, m)
+
+  def access_with(d, m)
     tmp_h = {}
-    h.each {|k, v|
-      v = access_with(v, m) if v.is_a? Hash
-      tmp_h[k.send(m)] = v
+    d.each {|k, v|
+      if v.is_a? Hash
+        tmp_h[k.send(m)] = access_with(v, m)
+      elsif v.is_a? Array
+        v_dup = v.dup
+        v_dup.each_with_index {|item, index| v_dup[index] = access_with(item, m) if item.is_a? Hash}
+        tmp_h[k.send(m)] = v_dup
+      else
+        tmp_h[k.send(m)] = v
+      end
     }
     tmp_h
   end
--- test/hash_with_double_access.rb
+++ test/hash_with_double_access.rb
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class HashWithDoubleAccessTest < Test::Unit::TestCase
 
   def test_double_access
-    h = {:a => 1, 'b' => 2, 'c' => {'d' => 4, :e => 5}}
+    h = {:a => 1, 'b' => 2, 'c' => {'d' => 4, :e => 5}, 'g' => [1,2,3], 'h' => [{'a' => 1, 'cc' => [{'b' => 2}, {'c' => 3}]}, {'b' => 5}]}
 
     hd = HashWithDoubleAccess h
 
@@ -18,6 +19,13 @@ class HashWithDoubleAccessTest < Test::Unit::TestCase
     assert_equal hd[:c]['e'], nil
     assert_equal hd['c'][:e], nil
     assert_equal hd['c'][:d], nil
+
+    assert_equal hd[:h].size, 2
+    assert_equal hd[:h][0][:a], 1
+    assert_equal hd[:h][1][:b], 5
+    assert_equal hd['h'][0][:a], nil
+    assert_equal hd['h'][1][:b], nil
+
   end
 
 end
