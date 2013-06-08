3296109bf2e91e9bd5a58986d46bdfdb2eb7b7a9
jiangguimin <jiangguimin@localhost.(none)>
2013-06-08 17:41:27 +0800

    json format

--- lib/ground/protocol/render.rb
+++ lib/ground/protocol/render.rb
@@ -4,15 +4,13 @@ module Ground::Protocol
     def haml(path)
       response['Content-Type'] = 'text/html; charset=UTF-8'
       template = Tilt::HamlTemplate.new(File.join(views, "#{path}.haml"))
-      output = template.render(self)
-      response.write(output)
+      response.write(template.render(self))
       response.finish
     end
 
     def json(content)
-      response['Content-Type'] = 'text/json; charset=UTF-8'
-      output = response.write(content.to_json)
-      response.write(output)
+      response['Content-Type'] = 'application/json; charset=UTF-8'
+      response.write(content)
       response.finish
     end
 
